9f3316c0905c89752da055d55fa94e1f788c206a
Travis Lee <travisslee223@gmail.com>
2020-10-21 20:16:08 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,5 +3,12 @@ class Puppy
   
   attr_accessor :name, :breed, :age 
   
-  def initialize()
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
 end
